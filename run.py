1908e83cc7a6b1fe594809c8ee206dff35b4d02c
strasse34 <>
2023-05-29 19:22:16 +0000

    still working on getting date from user. I want system check the date and if it before current date, system show an error to user.

--- run.py
+++ run.py
@@ -29,16 +29,9 @@ def check_format(day, date, current_year):
     """
     check format of provided date
     """
-    get_current_year()
-    get_day(date)
     try:
-        if date[2] != '.' or date[5] != '.':
+        if date[5] != '.':
             print("Invalid date format! Please provide date according to the format (dd.mm.yyyy)!")
-            get_date()
-        elif int(date[0:2]) > 31 or int(date[0:2]) == 0:
-            print("Invalid date format! Day must be a number in range: 1-31 !")
-        elif int(date[3:5]) > 12 or int(date[3:5]) == 0:
-            print("Invalid date format! Month must be a number in range: 1-12 !")
             get_date()
         elif int(date[6:10]) < int(current_year):
             print(f"Invalid date format! Year must be equal or greather than {current_year}.")
@@ -62,9 +55,13 @@ def get_day(date):
     """
     getting day according to the user date entery
     """
-    day = datetime.datetime.strptime(date, "%d.%m.%Y").strftime("%A")
-    return day
-
+    try:
+        day = datetime.datetime.strptime(date, "%d.%m.%Y").strftime("%A")
+        return day
+    except ValueError:
+        print('Invalid date format! Please Try again!')
+        get_date()
+    
 
 
 get_date()
