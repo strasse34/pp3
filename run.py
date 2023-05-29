510b47fd1f61bf05a6de11befd3f32747709f638
strasse34 <>
2023-05-29 15:18:03 +0000

    add: get_date funtion

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 import datetime
-import sys 
+import sys
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -15,42 +15,56 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('to_do_list')
 
 
-
 def get_date():
     """
     geting date from user according to the correct format.
     """
     date = input("Date(dd.mm.yyyy): ")
     current_year = get_current_year()
-    check_format(date, current_year)
+    day = get_day(date)
+    check_format(day, date, current_year)
 
 
-def check_format(date, current_year):
+def check_format(day, date, current_year):
     """
     check format of provided date
     """
     get_current_year()
-    if not date.isdigit():    
-        print("Invalid data! Please provide only numbers and .!")
-    elif len(date) != 10 or date[2] != '.' or date[5] != '.':
-        print("Invalid data! Please provide date according to (dd.mm.yyyy!")
-    elif int(date[0:2] > 31):
-        print("Invalid data! Day must be equal or less than 31!")
-    elif int(date[3:5] > 12):
-        print("Invalid data! Month must be equal or less than 12!")
-    elif int(date[6:10]) > int(current_year):
-        print(f"Invalid data! Month must be equal to {current_year}")
-    else:
-        print(f"{date} was saved.")
+    get_day(date)
+    try:
+        if date[2] != '.' or date[5] != '.':
+            print("Invalid date format! Please provide date according to the format (dd.mm.yyyy)!")
+            get_date()
+        elif int(date[0:2]) > 31 or int(date[0:2]) == 0:
+            print("Invalid date format! Day must be a number in range: 1-31 !")
+        elif int(date[3:5]) > 12 or int(date[3:5]) == 0:
+            print("Invalid date format! Month must be a number in range: 1-12 !")
+            get_date()
+        elif int(date[6:10]) < int(current_year):
+            print(f"Invalid date format! Year must be equal or greather than {current_year}.")
+            get_date()
+        else:
+            print(f"You selected this date: {day}, {date}")
+    except ValueError:
+        print("Invalid date format! Please Try again!")
+        get_date()
+   
 
 
 def get_current_year():
     """
-    gettin current year to check 
+    getting current year to check not belong to the past
     """
     current_year = datetime.date.today().year
     return current_year
 
+def get_day(date):
+    """
+    getting day according to the user date entery
+    """
+    day = datetime.datetime.strptime(date, "%d.%m.%Y").strftime("%A")
+    return day
+
+
 
 get_date()
-
