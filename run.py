6423d06376decd1c5fb8e1900fbbbad0e40d74a8
strasse34 <>
2023-06-11 14:46:10 +0000

    fix: problem with not saving data to google sheet was solved. I made an universal list and appended it by executing each function. Then passed it to Update_worksheet. Now code works fine.

--- run.py
+++ run.py
@@ -22,7 +22,6 @@ def get_date():
     """
     while True:
         entered_date = input("\nStep 1: setting date\nPlease set a date (dd.mm.yyyy): ")
-
         try:
             date_validation = datetime.datetime.strptime(entered_date, "%d.%m.%Y")
             selected_date = date_validation.strftime("%d.%m.%Y")
@@ -33,11 +32,19 @@ def get_date():
         except ValueError:
             print("\nInvalid date format! Please provide the date in the format dd.mm.yyyy!\n")
             continue
-
-    get_date_confirmation()
-    date = [selected_date, day_of_week]
-    return date
-
+    while True:
+        confirmation = input("Do you confirm it? (y/n): ")
+        if confirmation.lower() == 'y':
+            event_data.append(entered_date)
+            event_data.append(day_of_week)
+            print(event_data)
+            print('\nStep 2: setting time')
+            get_time()
+        elif confirmation.lower() == 'n':
+            get_date()
+        else:
+            print('\nPlease use "y" for Yes and "n" for No!')
+    
 
 def get_time():
     """
@@ -46,7 +53,6 @@ def get_time():
     """
     while True:
         entered_time = input("Please pick a time (Example: 14:30): ")
-        
         try:
             time_validation = datetime.datetime.strptime(entered_time, "%H:%M")
             valid_time = time_validation.strftime("%H:%M")
@@ -56,9 +62,19 @@ def get_time():
         except ValueError:
             print("\nInvalid time format! Please provide the time in the format hh:mm !\n")
             continue
-
-    get_time_confirmation()
-    return entered_time
+    while True:
+        confirmation = input("Do you confirm it? (y/n): ")
+        if confirmation.lower() == 'y':
+            event_data.append(entered_time)
+            print(event_data)
+            print('\nStep 3: selecting a title')
+            get_title()
+        elif confirmation.lower() == 'n':
+            print('\nStep 2: setting time')
+            get_time()
+        else:
+            print('\nPlease use "y" for Yes and "n" for No!')
+    
 
 
 def get_title():
@@ -66,18 +82,19 @@ def get_title():
     Getting Title for the event that user want to add to the list
     """
     entered_title = input("Please give your event a title!(Max 20 chrachters): ")[:20]
-    answer = input(f'\nYou provided this title "{entered_title}". Done? (y/n) ')
+    answer = input(f'\nYou provided this title "{entered_title}". Sure? (y/n) ')
     if answer.lower() == 'y':
+        event_data.append(entered_title)
+        print(event_data)
         print('\nStep 4: adding note')
         get_note()
-    elif answer.lower() == 'n':
+    elif answer.lower(event_data) == 'n':
         print('\nStep 3: selecting a title')
         get_title()
     else:
-        print('Please use "y" for yes and "n" for no')
+        print('Please use "y" for yes and "n" for no!')
         get_title()
-    return entered_title
-
+    
 
 
 def get_note():
@@ -85,57 +102,33 @@ def get_note():
     Getting Note for the tiltle that user has already selected.
     """
     entered_note = input("Please add a short note to the title!(Max 150 chrachters): \n")[:150]
-    answer = input(f'\nYou provided below note \n\n"{entered_note}"\n\n Done? (y/n) ')
+    answer = input(f'\nYou provided below note: \n\n"{entered_note}"\n\nSure? (y/n) ')
     if answer.lower() == 'y':
+        event_data.append(entered_note)
+        print(event_data)
         print('\nStep 5: saving data')
         save_date()
     elif answer.lower() == 'n':
         print('\nStep 4: adding note')
         get_note()
     else:
-        print('\nPlease use "y" for yes and "n" for no')
+        print('\nPlease use "y" for yes and "n" for no!')
         get_note()
-    return entered_note
+   
 
-
-def get_date_confirmation():
-    """
-    Getting confrimation of user about entered date
-    """
-    confirmation = input("Do you confirm it? (y/n): ")
-    if confirmation.lower() == 'y':
-        print('\nStep 2: setting time')
-        get_time()
-    elif confirmation.lower() == 'n':
-        get_date()
-    else:
-        print('\nPlease use "y" for Yes and "n" for No!')
-        get_date_confirmation()
-
-
-def get_time_confirmation():
-    """
-    Getting confrimation of user about entered time
-    """
-    confirmation = input("Do you confirm it? (y/n): ")
-    if confirmation.lower() == 'y':
-        print('\nStep 3: selecting a title')
-        get_title()
-    elif confirmation.lower() == 'n':
-        print('\nStep 2: setting time')
-        get_time()
-    else:
-        print('Please use "y" for Yes and "n" for No!')
-        get_date_confirmation()
-        
 
 def save_date():
     """
     Getting user confirmation for saving data or leading user to exit or edit date.
     """
-    confirmation = input("Please select one of the followings! (s = Save date / e = Exit)")
+    date = event_data[0]
+    day = event_data[1]
+    time = event_data[2]
+    title = event_data[3]
+    note = event_data[4]
+    print(f'\nYou provided below information for your event:\nDate: {date}\nDay: {day}\nTime: {time}\nTitle: {title}\nNote: {note}')
+    confirmation = input("\nPlease select one of the followings! (s = Save event / e = Exit)")
     if confirmation.lower() == 's':
-        event_data = collect_event_data()
         update_worksheet(event_data)
     elif confirmation.lower() == 'e':
         print('\nHave a nice day and goodbye!')
@@ -148,31 +141,18 @@ def save_date():
 
 
 
-def collect_event_data():
-    """
-    Collecting all the confirmed data in a list for adding to the google sheet.
-    """
-    confirmed_date = get_date()
-    confirmed_time = get_time()
-    confirmed_title = get_title()
-    confirmed_note = get_note()
-    event = confirmed_date + [confirmed_time, confirmed_title, confirmed_note]
-    print(event)
-    return event
-
-
-def update_worksheet():
+def update_worksheet(event_data):
     """
     Updating the user worksheet in google sheet with new to do list.
     """
-    event_data = collect_event_data()
-    print("Saving date ...\n")
+    print("\nSaving date ...\n")
     user_worksheet = SHEET.worksheet("josef_123")
     user_worksheet.append_row(event_data)
     print("New event successfully saved!\n")
+    new_event = input('\nIf you want to add an other event please Enter!')
+    if new_event == "":
+        get_date()
 
 
-
-
+event_data = []
 get_date()
-
